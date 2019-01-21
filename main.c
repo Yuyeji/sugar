3de5ab15c067ee25b8348e4684bf5f5a478ee240
Yuyeji <yeji3202@naver.com>
2019-01-21 15:24:02 +0900

    Update main.c

--- main.c
+++ main.c
@@ -9,27 +9,22 @@
 #include <stdio.h>
 
 int getMin(int num){
-    int result = 0;
+    int result = -1;
     
-    if(num%5 == 0){         //5묶음으로 떨어질 경우
+    if(num%5 == 0){
         result = num/5;
-    }else if(num%5 == 3){   //나머지가 3일 경우
-        int point = num/5;  //5묶음의 개수
-        result = point + (num - point*5)/3; //5묶음의 개수 + 3묶음의 개수
-    }else{                  //그 외
-        if(num%3 == 0){
-            result = num/3;
-        }else{
-            result = -1;
+    }else{
+        for(int i = num/5 ; i>=0; i--){
+            if((num-i*5)%3 == 0){
+                return i + (num-i*5)/3;
+            }
         }
     }
-    
-    
     return result;
 }
 
 int main(int argc, const char * argv[]) {
-    int N = 0;
+    int N;
     scanf("%d", &N);
     
     printf("%d\n" , (int) getMin(N));
